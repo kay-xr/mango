def60fb1383e45502c5eaff2eb87f96983d8f9fb
kay-xr <uhmkaylauh@gmail.com>
2025-04-03 15:38:50 -0400

    Init API

--- mango-web/src/lib/server/db/schema.ts
+++ mango-web/src/lib/server/db/schema.ts
@@ -15,6 +15,14 @@ export const session = pgTable('session', {
 	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
 });
 
+export const host = pgTable('host', {
+	id: text('id').primaryKey(),
+	address: text('address').notNull(),
+	port: integer('port').notNull(),
+	hostname: text('hostname').notNull(),
+	lastSeen: timestamp('last_seen', { withTimezone: true, mode: 'date' }),
+})
+
 export type Session = typeof session.$inferSelect;
 
 export type User = typeof user.$inferSelect;
